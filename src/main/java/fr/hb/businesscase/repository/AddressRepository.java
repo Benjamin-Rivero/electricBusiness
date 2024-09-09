ebc4c1a5a8328aece2b80fea77c35cf491e1b363
Benjamin-Rivero <benjamin.rivero24@gmail.com>
2024-09-09 14:02:56 +0200

    Added finding station location near the user

--- src/main/java/fr/hb/businesscase/repository/AddressRepository.java
+++ src/main/java/fr/hb/businesscase/repository/AddressRepository.java
@@ -4,6 +4,11 @@ import fr.hb.businesscase.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    List<Address> findByLongitudeBetweenAndLatitudeBetween(float longitude, float latitude, float longitude2, float latitude2);
+
 }
